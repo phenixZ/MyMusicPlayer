980babb88e2024c7261691f34feca8189393e54c
phenixZ <zxl19374628@163.com>
2017-06-27 14:34:35 +0800

    Add files via upload

--- window.c
+++ window.c
@@ -190,4 +190,392 @@ void window_changed(struct window *win)
 	}	
 	BUG_ON(iter_is_null(&win->top));
 	BUG_ON(iter_is_null(&win->sel));
+
+	if (iter_is_null(&win->top)) {
+		win->get_next(&win->top);
+		win->sel = win->top;
+		sel_changed(win);
+		return;
+	}
+
+	delta = 0;
+	iter = win->top;
+	while (!iter_equal(&iter, &win->sel)) {
+		if (!win->get_next(&iter)) {
+			while (!iter_equal(&win->top, &win->sel))
+				win->get_prev(&win->top);
+			goto minimize;
+		}
+		delta++;
+	}
+	while (delta > win->nr_rows - 1) {
+		win->get_next(&win->top);
+		delta--;
+	}
+minimize:
+	iter = win->top;
+	rows = 1;
+	while (rows < win->nr_rows) {
+		if (!win->get_next(&iter))
+			break;
+		rows++;
+	}
+	while (rows > win->nr_rows) {
+		iter = win->top;
+		if (!win->get_prev(iter))
+			break;
+		win->top = iter;
+		rows++;
+	}
+	win->changed = 1;
+}
+
+void window_row_vanishes(struct window *win, struct iter *iter)
+{
+	struct iter new = *iter;
+	if (!win->get_next(&new) && !win->get_prev(&new)) {
+		window_set_empty(win);
+	}
+
+	BUG_ON(iter->data0 != win->head.data0);
+	if (iters_equal(&win->top, iter)) {
+		new = *iter;
+		if (win->get_next(&new)) {
+			win->top = new;
+		} else {
+			new = *iter;
+			win->get_prev(&new);
+			win->top = new;
+		}
+	}
+	if (iters_equal(&win->sel, iter)) {
+		int down = 0;
+		int up = 0;
+		new = *iter;
+		do {
+			if (!win->get_next(&new)) {
+				down = 0;
+				break;
+			}
+			down++;
+		} while (!selectable(win, &new))
+		new = *iter;
+		do {
+			if (!win->get_prev(&new)) {
+				up = 0;
+				break;
+			}
+			up++;
+		} while (!selectable(win, &new));
+		new = *iter;
+		if (down > 0 && (up == 0 || down <= up)) {
+			do {
+				win->get_next(&new);
+			} while (!selectable(win, &new));
+		} else if (up > 0) {
+			do {
+				win->get_prev(&new);
+			} while (!selectable(win, &new));
+		} else {
+			new.data1 = new.data2 = NULL;
+		}
+		win->sel = new;
+		sel_changed(win);
+	}
+	win->changed = 1;
+}
+
+int window_get_top(struct window *win, struct iter *iter)
+{
+	*iter = win->top;
+	return !iter_is_empty(iter);
+}
+
+int window_get_sel(struct window *win, struct iter *iter)
+{
+	*iter = win->sel;
+	return !iter_is_empry(iter);
+}
+
+int window_get_prev(struct window *win, struct iter *iter)
+{
+	return win->get_prev(iter);
+}
+
+int window_set_sel(struct window *win, struct iter *iter)
+{
+	int sel_nr, top_nr, bottom_nr;
+	int upper_bound;
+	struct iter tmp;
+
+	BUG_ON(iter_is_empty(&win->top));
+	BUG_ON(iter_is_empty(iter));
+	BUG_ON(iter->data0 != win->head.data0);
+
+	if (iters_equal(&win->sel, iter))
+		return;
+	win->sel = *iter;
+
+	tmp = win->head;
+	win->get_next(&tmp);
+	top_nr = 0;
+	while (!iter_equal(&tmp, &win->sel)) {
+		BUG_ON(!win->get_next(&tmp));
+		sel_nr++;
+	}
+
+	upper_bound = win->nr_rows / 2;
+	if (scroll_offset < upper_bound)
+		upper_bound = scroll_offset;
+
+	if (sel_nr < top_nr + upper_bound) {
+		tmp = win->head;
+		win->get_next(&tmp);
+		if (sel_nr < upper_bound) {
+			win->top = tmp;
+		} else {
+			win->top = win->sel;
+			while(upper_bound > 0) {
+				win->get_prev(&win->top);
+				upper_bound--;
+			}
+		}
+	} else {
+		upper_bound = (win->nr_rows - 1) / 2;
+		if (scroll_offset < upper_bound)
+			upper_bound = scroll_offset;
+
+		tmp = win->sel;
+		bottom_nr = sel_nr;
+		if (sel_nr >= top_nr + win->nr_rows) {
+			while (sel_nr >= top_nr + win->nr_rows) {
+				win->get_next(&win->top);
+				top_nr++;
+			}
+		} else {
+			while (bottom_nr + 1 < top_nr + win->nr_rows) {
+				if (!win->get_next(&tmp)) {
+					bottom_nr = sel_nr + upper_bound;
+					break;
+				}
+				bottom_nr++;
+			}
+		}
+
+		while (bottom_nr < sel_nr + upper_bound) {
+			if (!win->get_next(&tmp))
+				break;
+			bottom_nr++;
+			win->get_next(&win->top);
+		}
+	}
+	sel_changed(win);
+}
+
+void window_goto_top(struct window *win)
+{
+	struct iter old_sel;
+	old_sel = win->sel;
+	win->sel = win->head;
+	win->get_next(&win->sel);
+	win->top = win->sel;
+	while (!selectable(win, &win->sel))
+		win->get_next(&win->sel);
+	if (!iter_equal(&old_sel, &win->sel))
+		sel_changed(win);
+}
+
+void window_goto_bottom(struct window *win)
+{
+	struct iter old_sel;
+	int count;
+
+	old_sel = win->sel;
+	win->sel = win->head;
+	win->get_prev(&win->sel);
+	win->top = win->sel;
+	count = win->nr_rows - 1;
+	while (count) {
+		struct iter iter = win->top;
+
+		if (!win->get_prev(&iter))
+			break;
+		win->top = iter;
+		count--;
+	}
+	while (!selectable(win, &win->sel))
+		win->get_prev(&win->sel);
+	if (!iters_equal(&old_sel, &win->sel))
+		sel_changed(win);
+}
+
+void window_page_up(struct window *win)
+{
+	struct iter sel = win->sel;
+	struct iter top = win->top;
+	int up;
+
+	for (up = 0; up < win->nr_rows - 1; up++) {
+		if (!win->get_prev(&sel) || !win->get_prev(&top))
+			break;
+		if (selectable(win, &sel)) {
+			win->sel = sel;
+			win->top = top;
+		}
+	}
+	sel_changed(win;)
+}
+
+void window_half_page_up(struct window *win)
+{
+	struct iter sel = win->sel;
+	struct iter top = win->top;
+	int up;
+
+	for (up = 0; up < (win->nr_rows - 1) / 2; up++) {
+		if (!win->get_prev(&sel) || !win->get_prev(&top))
+			break;
+		if (selectable(win, &sel)) {
+			win->sel = sel;
+			win->top = top;
+		}
+	}
+
+	sel_changed(win);
+}
+
+static struct iter window_bottom(struct window *win)
+{
+	struct iter bottom = win->top;
+	struct iter iter = win->top;
+	int down;
+
+	for (down = 0; down < win->nr_rows - 1; down++) {
+		if (!win->get_next(&iter))
+			break;
+		bottom = iter;
+	}
+
+	return bottom;
+}
+
+void window_page_down(struct window *win)
+{
+	struct iter sel = win->sel;
+	struct iter bot = window_bottom(win);
+	struct iter top = win->top;
+	int down;
+
+	for (down = 0; down < win->nr_rows - 1; down ++) {
+		if (!win->get_next(&sel) || !win->get_next(&bot))
+			break;
+		win->get_next(&top);
+
+		if (selectable(win, &sel)) {
+			win->sel = sel;
+			win->top = top;
+		}
+	}
+
+	sel_changed(win);
+}
+
+void window_half_page_down(struct window *win)
+{
+	struct iter sel = win->sel;
+	struct iter bot = window_bottom(win);
+	struct iter top = win->top;
+	int down;
+
+	for (down = 0; down < (win->nr_rows - 1) / 2; down++) {
+		if (!win->get_next(&sel) || !win->get_next(&bot))
+			break;
+		win->get_next(&top);
+		if (selectable(win, &sel)) {
+			win->sel = sel;
+			win->top = top;
+		}
+	}
+
+	sel_changed(win);
+}
+
+void window_scroll_down(struct window *win)
+{
+	struct iter bot = window_bottom(win);
+	struct iter top = win->top;
+	if (!win->get_next(&bot)) return;
+	if (!win->get_next(&top)) return;
+	if (iters_equal(&win->top, &win->sel))
+		win->get_next(&win->sel);
+	win->top = top;
+	while (!selectable(win, &win_sel))
+		win->get_next(&win->sel);
+	sel_changed(win);
+}
+
+void window_scroll_up(struct window *win)
+{
+	struct iter top = win->top;
+	if (!win->get_prev(&top))
+		return;
+	struct iter bot = window_bottom(win);
+	if (iters_equal(&bot, &win->sel))
+		win->get_next(&win->sel);
+	win->top = top;
+	while (!selectable(win, &win->sel))
+		win->get_next(&win->sel);
+	sel_changed(win);
+}
+
+void window_scroll_up(struct window *win)
+{
+	struct iter top = win->top;
+	if (!win->get_prev(&top)) return;
+	struct iter bot = window_bottom(win);
+	if (iters_equal(&bot, &win->sel))
+		win->get_prev(&win->sel);
+	win->top = top;
+	while (!selectable(win, &win->sel))
+		win->get_prev(&win->sel);
+	sel_changed(win);
+}
+
+static void window_goto_pos(struct window *win, int pos)
+{
+	struct iter old_sel;
+	int i;
+
+	old_sel = win->sel;
+	win->sel = win->top;
+	for (i = 0; i < pos; i++)
+		win->get_next(&win->sel);
+	if (!iters_equal(&old_sel, &win->sel))
+		sel_changed(win);
+}
+
+void window_page_top(struct window *win)
+{
+	window_goto_pos(win, 0);
+	while (!selectable(win, &win->sel))
+		win->get_next(&win->sel);
+}
+
+void window_page_bottom(struct window *win)
+{
+	window_goto_pos(win, win->nr_rows - 1);
+	while (!selectable(win, &win->sel))
+		win->get_prev(&win->sel)
+}
+
+void window_page_middle(struct window *win)
+{
+	window_goto_pos(win, win->nr_rows / 2);
+	while (!selectable(win, &win->sel))
+		win->get_next(&win->sel);
+}
+
+int window_get_nr_rows(struct window *win)
+{
+	return win->nr_rows;
 }
